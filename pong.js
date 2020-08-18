f8d5f8a7a5bc5e5bfcdf543ff7d50d864fac5350
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-18 12:14:27 +0200

    Verified by Cia V1.0.2

--- pong.js
+++ pong.js
@@ -1,1 +1,11 @@
 const canvas = document.getElementById("pong")
+
+const context = canvas.getContext("2d");
+context.filterStyle = "black"
+context.fillRect(100, 200, 50, 75);
+context.filterStyle = "red"
+context.beginPath();
+context.arc( 300, 350, 100, 0, Math.PI*2,false);
+
+console.log(context)
+
